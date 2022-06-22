070b34a6bc57584762e8520585abc49e094ee9a9
SushantTAN <punk1susant@gmail.com>
2022-06-22 12:47:05 +0545

    reanimated setup

--- src/components/modules/walkthrough/walkthrough.js
+++ src/components/modules/walkthrough/walkthrough.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import styles from "./styles";
+import Animated, { FadeIn } from 'react-native-reanimated';
 
 const Walkthrough = (props) => {
   return (
     <View style={styles.container}>
 
-      <Image
-        // style={styles.tinyLogo}
-        source={require('../../../assets/images/apexLogo.png')}
-      />
+      <Animated.View entering={FadeIn.delay(500).duration(700)}>
+        <Image
+          // style={styles.tinyLogo}
+          source={require('../../../assets/images/apexLogo.png')}
+        />
+      </Animated.View>
     </View>
   );
 }
